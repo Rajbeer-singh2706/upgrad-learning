227f2a2fb3f8bbe505aa9e574457e6dcdb2c0acc
raj singh <rajsingh@rajs-MacBook-Air.local>
2024-04-16 10:49:07 +0530

    updated

---
+++ module4:pythonfordatascience/pandas_5_hour/summary.sql
@@ -0,0 +1,6 @@
+Pandas Series and Dataframes, which are the basic data structures in the Pandas library
+Indexing, selecting and subsetting a dataframe
+Merging and appending two dataframes, which can be done using the .merge and .concat commands
+Grouping and summarising dataframes, which can be done using groupby() to first make an object and then use it to play around
+The pivot table function in a dataframe, which is similar to pivot tables in MS Excel
+Finally, you learnt how to perform different functions over time-series data using Pandas
